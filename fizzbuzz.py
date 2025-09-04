c93936665d249d3bb9dfc49dc427a04c2b465bc8
Abbi Portwood <abbiportwood@Abbis-MacBook-Air.local>
2025-09-04 12:46:33 -0400

    remove fizzbuzz

--- fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for i in range(101):
-    if i % 3 == 0 and i % 5 ==0:
-        print(i, "fizzbuzz")
-    elif i % 5 == 0:
-        print(i, "buzz")
-    elif i % 3 == 0:
-        print(i, "fizz")
-    else:
-        print(i)
