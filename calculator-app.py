14b40d0e3689cfe0862e67341ad2c7a573afa359
Abbi Portwood <abbiportwood@Abbis-MacBook-Air.local>
2025-09-04 18:56:32 -0400

    revision 2 calculaor-app

--- calculator-app.py
+++ calculator-app.py
@@ -3,7 +3,7 @@ def calculate(num1, num2, operation):
 # mathmatical operation with two number#
 # args: num1: first number, num2: second number, operation: +, -, *, /
 # returns: result of operation OR None if invalid
-
+            
     if operation == '+':
         return num1 + num2
 
@@ -20,31 +20,27 @@ def calculate(num1, num2, operation):
             print("Error: Division by zero not allowed.")  
             return None
 
-    elif operation == 'q':
-            return None
-
     else: 
-        print("Error: Invalaid operation.")
+        print("Error: Invalid operation.")
         return None
 
-#user input
-while calculate != None: 
+#user input 
+while True: 
     try: 
         number1 = float(input("Enter first number: "))
         number2 = float(input("Enter second number: "))
         operation = input("Enter operation symbol (or 'q' to quit): ")
-
+        if operation.lower() == 'q':
+            print("Goodbye!")
+            break
         result = calculate(number1, number2, operation)
-
-    except result is not None:
-        print(f"Result equalls: {result}")
+        if result is not None:
+            print(result)           
 
     except ValueError: 
         print("Invalid input. Enter valid numbers.")
 
-    if operation == 'q':
-        print("Goodbye!")
-        break
+
     
         
 
